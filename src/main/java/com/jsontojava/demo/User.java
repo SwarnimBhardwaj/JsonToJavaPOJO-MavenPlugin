0a0ebf190a340d16ea8e25599ca2f77a1bb8501a
Swarnim <swarnim2008@gmail.com>
2020-05-01 22:02:58 +0200

    First commit JsonTojava Maven Plugin

---
+++ src/main/java/com/jsontojava/demo/User.java
@@ -0,0 +1,20 @@
+package com.jsontojava.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+
+}
